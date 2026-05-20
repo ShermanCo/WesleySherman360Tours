2cda772dca0f475a0de77466accca3b1b4b0a99f
Wesley Sherman <186631423+WesleySherman@users.noreply.github.com>
2026-05-20 11:08:55 +0200

    Add tour: mill-park

---
+++ mill-park/data.js
@@ -0,0 +1,443 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---front-of-property",
+      "name": "01 - Front of Property",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.30921046318451495,
+        "pitch": 0.09845043907122886,
+        "fov": 1.4009841842570447
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.29314302075190035,
+          "pitch": 0.25304787598889966,
+          "rotation": 0,
+          "target": "1-02---reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---reception",
+      "name": "02 - Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.4728294660784087,
+        "pitch": 0.03566973969246945,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7889619717129754,
+          "pitch": 0.23069474955399372,
+          "rotation": 0,
+          "target": "0-01---front-of-property"
+        },
+        {
+          "yaw": 2.049364317869485,
+          "pitch": 0.2944657913695057,
+          "rotation": 0,
+          "target": "2-03---large-office-01"
+        },
+        {
+          "yaw": -0.9833813439598238,
+          "pitch": 0.33058915033447356,
+          "rotation": 7.853981633974483,
+          "target": "3-04---office-02"
+        },
+        {
+          "yaw": -1.1270504822684835,
+          "pitch": 0.32809064343566874,
+          "rotation": 4.71238898038469,
+          "target": "5-06---office-04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---large-office-01",
+      "name": "03 - Large Office 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.5537780046074783,
+        "pitch": 0.03050586113828757,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7637375301954838,
+          "pitch": 0.25984438761305206,
+          "rotation": 0,
+          "target": "1-02---reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---office-02",
+      "name": "04 - Office 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.7872972754832501,
+        "pitch": 0.043814318733204516,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.397266588526339,
+          "pitch": 0.3791484789263695,
+          "rotation": 1.5707963267948966,
+          "target": "6-07---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---office-03",
+      "name": "05 - Office 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.6098429951736986,
+        "pitch": -0.01814857613439358,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.084257268461414,
+          "pitch": 0.39637404166991885,
+          "rotation": 1.5707963267948966,
+          "target": "8-09---toilet"
+        },
+        {
+          "yaw": 2.9489209932171967,
+          "pitch": 0.4248904415477064,
+          "rotation": 4.71238898038469,
+          "target": "4-05---office-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---office-04",
+      "name": "06 - Office 04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.2439165433396422,
+        "pitch": -0.005659932249610478,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0611772130227752,
+          "pitch": 0.4590997261423695,
+          "rotation": 12.566370614359176,
+          "target": "8-09---toilet"
+        },
+        {
+          "yaw": 2.015025138598423,
+          "pitch": 0.3700878359985431,
+          "rotation": 6.283185307179586,
+          "target": "7-08---bathroom"
+        },
+        {
+          "yaw": 1.9674096918504178,
+          "pitch": 0.27569571111666313,
+          "rotation": 6.283185307179586,
+          "target": "6-07---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07---kitchen",
+      "name": "07 - Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.3684249522847356,
+        "pitch": -0.03924534643727284,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1440972746240377,
+          "pitch": 0.43713225042232473,
+          "rotation": 1.5707963267948966,
+          "target": "7-08---bathroom"
+        },
+        {
+          "yaw": -2.358338892389245,
+          "pitch": 0.47526669319995385,
+          "rotation": 4.71238898038469,
+          "target": "3-04---office-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08---bathroom",
+      "name": "08 - Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.6219907259725943,
+        "pitch": 0.014426764037594353,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5412021965837717,
+          "pitch": 0.8614325117076032,
+          "rotation": 7.853981633974483,
+          "target": "4-05---office-03"
+        },
+        {
+          "yaw": 2.2147476157263046,
+          "pitch": 0.7081484012153147,
+          "rotation": 4.71238898038469,
+          "target": "6-07---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09---toilet",
+      "name": "09 - Toilet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.3967566780488774,
+        "pitch": 0.5168263431213038,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0217858577894976,
+          "pitch": 0.86123525009784,
+          "rotation": 1.5707963267948966,
+          "target": "5-06---office-04"
+        },
+        {
+          "yaw": 2.3046562164137985,
+          "pitch": 0.7978126488070458,
+          "rotation": 4.71238898038469,
+          "target": "4-05---office-03"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mill Park",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
