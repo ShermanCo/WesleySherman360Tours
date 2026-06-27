7f8f9529c26101583e44cf34aeb6e2b72629529c
Wesley Sherman <186631423+WesleySherman@users.noreply.github.com>
2026-06-27 11:42:06 +0200

    Add tour: bridgewater-unit-55

---
+++ bridgewater-unit-55/data.js
@@ -0,0 +1,635 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---complex-entrance",
+      "name": "01 - Complex Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.06854383971468181,
+        "pitch": 0.03139627714982396,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18977982340124555,
+          "pitch": 0.1678655033087466,
+          "rotation": 1.5707963267948966,
+          "target": "12-13---parking-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---entrance",
+      "name": "02 - Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.785398498171908,
+        "pitch": -0.0003435759189827081,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.05778529617919226,
+          "pitch": -0.16661693446366854,
+          "rotation": 0,
+          "target": "12-13---parking-area"
+        },
+        {
+          "yaw": -3.1127908811123355,
+          "pitch": 0.13356940188355537,
+          "rotation": 0,
+          "target": "2-03---lounge"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---lounge",
+      "name": "03 - Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.6757879245469667,
+        "pitch": 0.01964587885733593,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1844297734604368,
+          "pitch": 0.12113338490708969,
+          "rotation": 0,
+          "target": "1-02---entrance"
+        },
+        {
+          "yaw": 2.70103079399452,
+          "pitch": 0.12160089600572732,
+          "rotation": 0,
+          "target": "3-04---kitchen"
+        },
+        {
+          "yaw": 3.105115245567357,
+          "pitch": 0.28106048783549653,
+          "rotation": 0.7853981633974483,
+          "target": "5-06---passage"
+        },
+        {
+          "yaw": -0.8458191935387855,
+          "pitch": 0.3699795414223672,
+          "rotation": 0,
+          "target": "4-05---patio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---kitchen",
+      "name": "04 - Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.6946276690316004,
+        "pitch": 0.0008934399090492207,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4655999160400626,
+          "pitch": 0.2057359892134425,
+          "rotation": 0,
+          "target": "2-03---lounge"
+        },
+        {
+          "yaw": -2.889295055178133,
+          "pitch": 0.37457714427358724,
+          "rotation": 4.71238898038469,
+          "target": "5-06---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---patio",
+      "name": "05 - Patio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.5886908212425634,
+        "pitch": -0.014380963134620117,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7373054985005707,
+          "pitch": 0.3119258025324534,
+          "rotation": 0,
+          "target": "2-03---lounge"
+        },
+        {
+          "yaw": -0.4405880074251449,
+          "pitch": 0.4542535287823579,
+          "rotation": 0,
+          "target": "11-12---exterior"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---passage",
+      "name": "06 - Passage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.3550015665013557,
+        "pitch": -0.003860222740385666,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.40477351785561133,
+          "pitch": 0.1986664669697049,
+          "rotation": 0,
+          "target": "6-07---main-bedroom"
+        },
+        {
+          "yaw": -0.7207864511420858,
+          "pitch": 0.6659599819156199,
+          "rotation": 0,
+          "target": "9-10---bathroom-2"
+        },
+        {
+          "yaw": -1.2396792358673565,
+          "pitch": 0.3679778302337944,
+          "rotation": 0,
+          "target": "8-09---bedroom-2"
+        },
+        {
+          "yaw": 0.9195430011020065,
+          "pitch": 0.7927941036245265,
+          "rotation": 1.5707963267948966,
+          "target": "2-03---lounge"
+        },
+        {
+          "yaw": -2.54172317611712,
+          "pitch": 0.5972984473148326,
+          "rotation": 0,
+          "target": "10-11---bedroom-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07---main-bedroom",
+      "name": "07 - Main Bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.8832288645853055,
+        "pitch": -0.006145626928457659,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7811070306343826,
+          "pitch": 0.20220998650405875,
+          "rotation": 0,
+          "target": "5-06---passage"
+        },
+        {
+          "yaw": 2.8449727172681287,
+          "pitch": 0.4125563444413629,
+          "rotation": 1.5707963267948966,
+          "target": "7-08---main-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08---main-bathroom",
+      "name": "08 - Main Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.4771142015832588,
+        "pitch": 0.8766332587717756,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0176924879844176,
+          "pitch": 0.7793901990161878,
+          "rotation": 1.5707963267948966,
+          "target": "5-06---passage"
+        },
+        {
+          "yaw": 2.6198450549756105,
+          "pitch": 0.8206304593325839,
+          "rotation": 4.71238898038469,
+          "target": "6-07---main-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09---bedroom-2",
+      "name": "09 - Bedroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.5790651716861284,
+        "pitch": 0.03069205400134223,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.006986412227173,
+          "pitch": 0.4336362584914717,
+          "rotation": 0,
+          "target": "5-06---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10---bathroom-2",
+      "name": "10 - Bathroom 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.5909692876393038,
+        "pitch": 0.6673068226307635,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.99725513130931,
+          "pitch": 0.7372433941211884,
+          "rotation": 0,
+          "target": "5-06---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11---bedroom-3",
+      "name": "11 - Bedroom 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.0333917780220538,
+        "pitch": -0.007606292789152036,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.614039304138073,
+          "pitch": 0.4520028875874438,
+          "rotation": 0,
+          "target": "5-06---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-12---exterior",
+      "name": "12 - Exterior",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.6575302972580772,
+        "pitch": 0.04347522376275492,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2879903165822171,
+          "pitch": 0.43258361357517217,
+          "rotation": 0,
+          "target": "4-05---patio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-13---parking-area",
+      "name": "13 - Parking Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.9137931625792319,
+        "pitch": 0.04476226493763846,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3441353562432319,
+          "pitch": 0.23729771540148903,
+          "rotation": 5.497787143782138,
+          "target": "1-02---entrance"
+        },
+        {
+          "yaw": -1.8733608559403123,
+          "pitch": -0.043358113451688496,
+          "rotation": 4.71238898038469,
+          "target": "0-01---complex-entrance"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Bridgewater",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
