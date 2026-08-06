7ae0eca636368a24922d66e19754360d6e8954a1
Wesley Sherman <186631423+WesleySherman@users.noreply.github.com>
2026-08-06 22:13:03 +0200

    Add tours: shop-6b-brookes-on-the-bay, shop-6c-brookes-on-the-bay

---
+++ shop-6b-brookes-on-the-bay/data.js
@@ -0,0 +1,299 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---front-of-shop",
+      "name": "01 - Front of Shop",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.5530250497801639,
+        "pitch": -0.003926063817289105,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0232153505033175,
+          "pitch": 0.16880857994872223,
+          "rotation": 0,
+          "target": "2-03---shop"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---shop",
+      "name": "02 - Shop",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.6044207347874169,
+        "pitch": -0.023317959659618026,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3591485652354844,
+          "pitch": 0.25127033776101015,
+          "rotation": 0,
+          "target": "2-03---shop"
+        },
+        {
+          "yaw": 2.5220353876121386,
+          "pitch": 0.180531809869521,
+          "rotation": 0,
+          "target": "3-04---shop"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---shop",
+      "name": "03 - Shop",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.2703685866832224,
+        "pitch": 0.041861702866413,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06692449041837989,
+          "pitch": 0.19296547414963072,
+          "rotation": 0,
+          "target": "0-01---front-of-shop"
+        },
+        {
+          "yaw": -0.9907319173015008,
+          "pitch": 0.283406346259266,
+          "rotation": 0,
+          "target": "1-02---shop"
+        },
+        {
+          "yaw": -2.137481434359824,
+          "pitch": 0.1889079100867299,
+          "rotation": 0,
+          "target": "3-04---shop"
+        },
+        {
+          "yaw": -3.099416736815371,
+          "pitch": 0.21637615912801955,
+          "rotation": 0,
+          "target": "5-06---bathroom"
+        },
+        {
+          "yaw": -2.457362596137095,
+          "pitch": 0.21584911757713598,
+          "rotation": 0,
+          "target": "4-05---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---shop",
+      "name": "04 - Shop",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.7624814841829313,
+        "pitch": 0.06287169264168746,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0489429850270433,
+          "pitch": 0.1985330864111674,
+          "rotation": 0,
+          "target": "2-03---shop"
+        },
+        {
+          "yaw": 2.336977276250253,
+          "pitch": 0.21975527066248546,
+          "rotation": 0,
+          "target": "1-02---shop"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---kitchen",
+      "name": "05 - Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.7431173540158067,
+        "pitch": -0.03158824411962868,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6079807202274665,
+          "pitch": 0.2836515861584026,
+          "rotation": 0,
+          "target": "2-03---shop"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---bathroom",
+      "name": "06 - Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.5714587529798187,
+        "pitch": 0.1194749227585099,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.826310673006045,
+          "pitch": 0.3058037657104009,
+          "rotation": 0,
+          "target": "2-03---shop"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Shop 6B, Brookes on the Bay",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ shop-6c-brookes-on-the-bay/data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---shop-6c-brookes-on-the-bay",
+      "name": "01 - Shop 6C, Brookes on the Bay",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.037146265839920645,
+        "pitch": 0.003949753769378006,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2900452371030795,
+          "pitch": 0.1891947600820778,
+          "rotation": 0,
+          "target": "1-02---shop-6c-brookes-on-the-bay"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---shop-6c-brookes-on-the-bay",
+      "name": "02 - Shop 6C, Brookes on the Bay",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.971415396576397,
+        "pitch": 0.05431663244221241,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.489207955982682,
+          "pitch": 0.19120279062974532,
+          "rotation": 0,
+          "target": "0-01---shop-6c-brookes-on-the-bay"
+        },
+        {
+          "yaw": -2.30448543768774,
+          "pitch": 0.3871533983151565,
+          "rotation": 0,
+          "target": "2-03---shop-6c-brookes-on-the-bay"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---shop-6c-brookes-on-the-bay",
+      "name": "03 - Shop 6C, Brookes on the Bay",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.016183962154862286,
+        "pitch": 0.09811336609320165,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2605660633168547,
+          "pitch": 0.3706453144542685,
+          "rotation": 0,
+          "target": "1-02---shop-6c-brookes-on-the-bay"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Shop 6C, Brookes on the Bay",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
