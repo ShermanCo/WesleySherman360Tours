d631014cedd17f5ab091c147cb167e80ba92eee3
Wesley Sherman <186631423+WesleySherman@users.noreply.github.com>
2026-08-07 08:01:47 +0200

    Add tours: shop-22-summerstrand-village, shop-28-summerstrand-village

---
+++ shop-22-summerstrand-village/data.js
@@ -0,0 +1,251 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---shop-22-summerstrand-village1",
+      "name": "01 - Shop 22, Summerstrand Village1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.09234378406005916,
+        "pitch": 0.02354720786239639,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.541128456039818,
+          "pitch": 0.23655731264605606,
+          "rotation": 0,
+          "target": "1-02---shop-22-summerstrand-village"
+        },
+        {
+          "yaw": -0.09218230890983214,
+          "pitch": 0.19933422029459003,
+          "rotation": 0,
+          "target": "2-03---shop-22-summerstrand-village"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---shop-22-summerstrand-village",
+      "name": "02 - Shop 22, Summerstrand Village",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.674134224227955,
+        "pitch": 0.03809228072150006,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6289807011281683,
+          "pitch": 0.23499088980651095,
+          "rotation": 0,
+          "target": "0-01---shop-22-summerstrand-village1"
+        },
+        {
+          "yaw": -2.7480173166663064,
+          "pitch": 0.3338241697329014,
+          "rotation": 0,
+          "target": "2-03---shop-22-summerstrand-village"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---shop-22-summerstrand-village",
+      "name": "03 - Shop 22, Summerstrand Village",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.47763594063142,
+        "pitch": 0.044515394987342916,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.124142380751133,
+          "pitch": 0.17835931480709277,
+          "rotation": 0,
+          "target": "0-01---shop-22-summerstrand-village1"
+        },
+        {
+          "yaw": 2.239385923037867,
+          "pitch": 0.33064376289023656,
+          "rotation": 0,
+          "target": "1-02---shop-22-summerstrand-village"
+        },
+        {
+          "yaw": -0.4911124943520164,
+          "pitch": 0.3434080377979747,
+          "rotation": 0,
+          "target": "3-04---shop-22-summerstrand-village"
+        },
+        {
+          "yaw": 0.6634491706544612,
+          "pitch": 0.41320579369488897,
+          "rotation": 5.497787143782138,
+          "target": "4-05---shop-22-summerstrand-village"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---shop-22-summerstrand-village",
+      "name": "04 - Shop 22, Summerstrand Village",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 3.125288110712873,
+        "pitch": 0.02078346617597404,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.401248197760868,
+          "pitch": 0.6181727094584719,
+          "rotation": 4.71238898038469,
+          "target": "2-03---shop-22-summerstrand-village"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---shop-22-summerstrand-village",
+      "name": "05 - Shop 22, Summerstrand Village",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.0126078879876577,
+        "pitch": 0.024361795317002333,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.67373678610344,
+          "pitch": 0.41381059276122123,
+          "rotation": 0,
+          "target": "2-03---shop-22-summerstrand-village"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Shop 22, Summerstrand Village",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
---
+++ shop-28-summerstrand-village/data.js
@@ -0,0 +1,95 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---shop-28-summerstrand-village",
+      "name": "01 - Shop 28, Summerstrand Village",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 3.018592382170884,
+        "pitch": 0.02927354595062681,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.088325652790571,
+          "pitch": 0.28427987215053285,
+          "rotation": 0,
+          "target": "1-02---shop-28-summerstrand-village"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---shop-28-summerstrand-village",
+      "name": "02 - Shop 28, Summerstrand Village",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.7019480301022654,
+        "pitch": 0.042360737402599824,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4365936275022904,
+          "pitch": 0.2834390419641011,
+          "rotation": 0,
+          "target": "0-01---shop-28-summerstrand-village"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Dhop 28, Summerstrand Village",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
