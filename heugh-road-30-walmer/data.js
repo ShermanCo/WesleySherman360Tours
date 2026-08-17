03732a6e28b56e6a65e938775677c801bb36d755
Wesley Sherman <186631423+WesleySherman@users.noreply.github.com>
2026-08-17 13:18:05 +0200

    Add tour: heugh-road-30-walmer

---
+++ heugh-road-30-walmer/data.js
@@ -0,0 +1,551 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---reception",
+      "name": "01 - Reception",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.075092176251106,
+        "pitch": -0.020180582506867495,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.36995242658088756,
+          "pitch": 0.4462684793140994,
+          "rotation": 0,
+          "target": "3-04---passage"
+        },
+        {
+          "yaw": -1.6485456161403107,
+          "pitch": 0.31105153125910334,
+          "rotation": 0,
+          "target": "2-03---boardroom"
+        },
+        {
+          "yaw": 2.2415568545334352,
+          "pitch": 0.28997285203813306,
+          "rotation": 0,
+          "target": "1-02---office-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---office-01",
+      "name": "02 - Office 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.4280231571521131,
+        "pitch": 0.11841456120506066,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3449108784072816,
+          "pitch": 0.3305734983455224,
+          "rotation": 0,
+          "target": "0-01---reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---boardroom",
+      "name": "03 - Boardroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.7643616191898523,
+        "pitch": 0.014211151235095087,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1482359252683167,
+          "pitch": 0.3046922420118978,
+          "rotation": 0,
+          "target": "0-01---reception"
+        },
+        {
+          "yaw": -3.053447939854305,
+          "pitch": 0.3198320619857693,
+          "rotation": 0,
+          "target": "3-04---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---passage",
+      "name": "04 - Passage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.2732961135248537,
+        "pitch": 0.03904412110321687,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20623183389055555,
+          "pitch": 0.8092631518229609,
+          "rotation": 5.497787143782138,
+          "target": "7-08---common-area"
+        },
+        {
+          "yaw": 0.3795200845519755,
+          "pitch": 0.30444409044818954,
+          "rotation": 0,
+          "target": "5-06---bathroom-02"
+        },
+        {
+          "yaw": 0.08654508823747342,
+          "pitch": 0.4312796966192476,
+          "rotation": 5.497787143782138,
+          "target": "6-07---office-03"
+        },
+        {
+          "yaw": -2.8006892344468657,
+          "pitch": 0.3456176195778493,
+          "rotation": 0,
+          "target": "4-05---bathroom-01"
+        },
+        {
+          "yaw": 2.9820449436264163,
+          "pitch": 0.5456258576759261,
+          "rotation": 5.497787143782138,
+          "target": "2-03---boardroom"
+        },
+        {
+          "yaw": 1.5627059313163096,
+          "pitch": 0.4609856231764269,
+          "rotation": 0,
+          "target": "0-01---reception"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---bathroom-01",
+      "name": "05 - Bathroom 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.20154250369042614,
+        "pitch": 0.34667000699471373,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7931485417368425,
+          "pitch": 0.4259378578767876,
+          "rotation": 0,
+          "target": "3-04---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---bathroom-02",
+      "name": "06 - Bathroom 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.4788560989434316,
+        "pitch": 0.10334775363574522,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.525909162303286,
+          "pitch": 0.42935557634973165,
+          "rotation": 0,
+          "target": "3-04---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07---office-03",
+      "name": "07 - Office 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.19705244021951884,
+        "pitch": 0.09026429680574566,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.226807364515011,
+          "pitch": 0.4018262285257794,
+          "rotation": 0,
+          "target": "9-10---storeroom"
+        },
+        {
+          "yaw": 1.9905255045255776,
+          "pitch": 0.45846114978777486,
+          "rotation": 5.497787143782138,
+          "target": "3-04---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08---common-area",
+      "name": "08 - Common Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.09900776847676696,
+        "pitch": -0.015698138574908427,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07284618287341793,
+          "pitch": 0.36749099317654554,
+          "rotation": 6.283185307179586,
+          "target": "10-11---kitchen"
+        },
+        {
+          "yaw": -1.7465755329483166,
+          "pitch": 0.4120723704192404,
+          "rotation": 0,
+          "target": "8-09---office-04"
+        },
+        {
+          "yaw": 2.6980474106656143,
+          "pitch": 0.4379597616198616,
+          "rotation": 0,
+          "target": "3-04---passage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09---office-04",
+      "name": "09 - Office 04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.147401253567331,
+        "pitch": 0.03926874590295171,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4407010750662455,
+          "pitch": 0.41062625001220177,
+          "rotation": 0,
+          "target": "7-08---common-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10---storeroom",
+      "name": "10 - Storeroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.4240877371894403,
+        "pitch": -0.004897259655265884,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.943733738047489,
+          "pitch": 0.34166275337579677,
+          "rotation": 0,
+          "target": "10-11---kitchen"
+        },
+        {
+          "yaw": 0.8408778657242877,
+          "pitch": 0.421806575449164,
+          "rotation": 0,
+          "target": "6-07---office-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11---kitchen",
+      "name": "11 - Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.6389206894308774,
+        "pitch": 0.025481490409585916,
+        "fov": 1.4010588678110822
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.223090727570739,
+          "pitch": 0.36031637394884797,
+          "rotation": 0,
+          "target": "9-10---storeroom"
+        },
+        {
+          "yaw": 2.844883681881509,
+          "pitch": 0.5628571481501545,
+          "rotation": 0.7853981633974483,
+          "target": "7-08---common-area"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Heugh Road 30, Walmer",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
